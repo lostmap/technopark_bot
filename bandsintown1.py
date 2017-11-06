5f886ebf4a321b1688f4c5ddce771c3318ba67a5
ryabkryab <32486576+ryabkryab@users.noreply.github.com>
2017-11-06 03:22:41 +0300

    Edited genre search абсолютли

--- bandsintown1.py
+++ bandsintown1.py
@@ -6,60 +6,23 @@ from bandsintown import Client
 client = Client('technopark_ruliiiit')
 
 
-
-
-# #GET
-#
-# # Find an artist by name
-#
-# client.get('Bad Religion')
-#
-# # Find an artist by Facebook page ID
-#
-# client.get(fbid=168803467003)
-#
-# # Find an artist by MusicBrainz ID
-#
-# client.get(mbid='149e6720-4e4a-41a4-afca-6d29083fc091')
-#
-#
-#
-# #Events
-#
-# client.events('Bad Religion')
-#
-# # Filter by date
-#
-# client.events('Bad Religion', date='2015-08-30')
-#
-# # ...or a date range
-#
-# # Filter by date
-#
-# client.events('Bad Religion', date='2015-08-30,2015-12-25')
-
-
-
-
-#Search
-
-
-
 def create_message(events):
-    message = ''
+
+    messages = list()
     print(events)
     for event in events:
-        message += "Title: "+ event['title']+"\n"  +"Facebook: "+ event['facebook_rsvp_url']
+        message = list()
         # + "Ticket status: "+ event['ticket_status'] + "\n"
-
         # print("Ticket type: ", event['ticket_type'])
-        # print("Formated datetime: ", event['formatted_datetime'])
-        # print("Datetime: ", event['datetime'])
+        message.append("Event date: " + event['formatted_datetime'])
+        # message += "Datetime: "+ event['datetime']
         # print("Formatted location: ", event['formatted_datetime'])
-        # print("Ticket url: ", event['ticket_url'])
+        if (event['ticket_url']):
+            message.append("Ticket url: " + event['ticket_url'])
         # print("Ticket status: ", event['ticket_status'])
-        # for artist in event['artists']:
-        #     print("Artist: ", artist['name'])
+        for artist in event['artists']:
+            if (artist['name']):
+                message.append("Artist: "+ artist['name'])
         #     print("Facebook page: ", artist['facebook_page_url'])
         #     print("Url: ", artist['url'])
         #     print("Thumb url: ", artist['thumb_url'])
@@ -72,15 +35,10 @@ def create_message(events):
         # print("Name: ", venue['name'])
         # print("Place: ", venue['place'])
         # print("Region: ", venue['region'])
-    return message
+        messages.append("\n".join(message))
+    messages.append("Title: " + event['title'] + "\n" + event['facebook_rsvp_url'])
+    return messages
 
-
- #   print("Artist: ", srch['artists'])
-
-# Pass an optional radius (in miles)
-#
-# client.search('Bad Religion', location='Portland,OR', radius=100)
-#
 
 #Recomended
 # events = client.recommended('Thirty Seconds to Mars', location='Moscow, Ru')
@@ -110,8 +68,3 @@ def create_message(events):
 #     print("Place: ", venue['place'])
 #     print("Region: ", venue['region'])
 
-
-# # Only show recommendations
-#
-# client.recommended('Rise Against', location='Portland,OR', only_recs=True)
-#
--- telega.py
+++ telega.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
-import requests
-import urllib
+
+import musicgraph1 as mg
 
 import bandsintown1 as bit
 
@@ -13,28 +13,106 @@ token = '403882463:AAGFabioSaA1uY5Iku7v-lXVJegeIoP-J3E'
 bot = telebot.TeleBot(token)
 
 
+#кнопка для жанров
+def genre_buttons(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*[types.InlineKeyboardButton(text=genre, callback_data=genre) for genre in ['Rock', 'Alternative/Indie', 'Pop',
+                                                                                               'Jazz', 'Soul/R&B', 'Blues',
+                                                                                              'Rap/Hip Hop', 'Folk']])
+    bot.send_message(message.chat.id, "Какой жанр выберешь?", reply_markup=keyboard)
+
+#для того же
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    call.message.text = '/genre ' + call.data
+    event_search_by_genre(call.message)
+
+
+# чисто чтоб обновить список комманд или сообщения, крч нужная штука
+@bot.message_handler(commands=['start'])
+def event_search_by_genre(message):
+    pass
+
+
+# #для /genre жанр
+# @bot.message_handler(commands=['genre'])
+# def event_search_by_genre(message):
+#     my_artists = mg.get_by_genre(message.text[7:])
+#     bot.send_message(message.chat.id, "\n".join(my_artists))
+#     for artist in my_artists:
+#         events = client.events(artist)
+#         if events:
+#             my_messages = bit.create_message(events)
+#             for my_message in my_messages:
+#                 bot.send_message(message.chat.id, my_message, parse_mode='HTML')
+
+# для /genre жанр, город
+@bot.message_handler(commands=['genre'])
+def event_search_by_genre(message):
+    if len(message.text) == 6:
+        genre_buttons(message)
+    else:
+        params = message.text.split(",")
+        params[0] = params[0][7:].strip()
+
+        if len(params) == 2:
+            my_artists = mg.get_by_genre(params[0])
+            bot.send_message(message.chat.id, "\n".join(my_artists))
+            for artist in my_artists:
+                events = client.search(artist, location=params[1].strip())
+                if events:
+                    my_messages = bit.create_message(events)
+                    for my_message in my_messages:
+                        bot.send_message(message.chat.id, my_message, parse_mode='HTML')
+        else:
+            my_artists = mg.get_by_genre(params[0])
+            bot.send_message(message.chat.id, "\n".join(my_artists))
+            for artist in my_artists:
+                events = client.events(artist)
+                if events:
+                    my_messages = bit.create_message(events)
+                    for my_message in my_messages:
+                        bot.send_message(message.chat.id, my_message, parse_mode='HTML')
+
+
+
+# поиск по датам в дипозоне))
+# @bot.message_handler(commands=['date'])
+# def date_search(message):
+#     params = message.text[6:]
+#     events = client.search(params,  location='Moscow,Ru', date='2017-12-30,2018-03-25')
+#     print(events)
+    # if events:
+    #     my_messages = bit.create_message(events)
+    #     for my_message in my_messages:
+    #         bot.send_message(message.chat.id, my_message, parse_mode='HTML')
+
+
+
+# для ввода Артист, Город
 @bot.message_handler(content_types=["text"])
-def handle_message(message):
-    events = client.events(message.text)
-    my_message = bit.create_message(events)
-    bot.send_message(message.chat.id, my_message)
-
-    # send_photo(message)
-
-
-#
-# def send_photo(message):
-#     bot.send_chat_action(message.chat.id, 'upload_photo')
-#     bot.send_photo(message.chat.id, )
+def artist_search(message):
+    params = message.text.split(",")
+    if len(params) == 2:
+        events = client.search(params[0].strip(), location=params[1].strip())
+    else:
+        events = client.events(params[0])
+    print(events)
+    my_messages = bit.create_message(events)
+    for my_message in my_messages:
+        bot.send_message(message.chat.id, my_message, parse_mode='HTML')
 
 
 
 
 
 
-
-
-
+# @bot.message_handler(content_types=["text"])
+# def recommendations(message):
+#     events = client.recommended(message.text, location='Moscow,Ru', only_recs=True)
+#     my_messages = bit.create_message(events)
+#     for my_message in my_messages:
+#         bot.send_message(message.chat.id, my_message)
 
 
 
