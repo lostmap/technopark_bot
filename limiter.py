4707936f4a19821b5bab8f634278f17278f9e259
lostmap <lostmap4@gmail.com>
2017-12-07 15:35:29 +0300

    before merge

--- limiter.py
+++ limiter.py
@@ -1,8 +1,10 @@
 import time
 
+
+#TO DO limiter 
 class RateLimiter:
     """
-    chat_id => last_send_time
+    chat_id => [last_send_time, last_minute_time, msg_count]
     """
     def __init__(self):
         self.users = {}
@@ -10,12 +12,29 @@ class RateLimiter:
     def can_send_to(self, chat_id):
         now = time.time()
         if not chat_id in self.users:
-            self.users[chat_id] = now
+            self.users[chat_id] = [now, now, 0]
             return True
 
-        # 1 message per second 
-        diff = now - self.users[chat_id]
-        if diff <= 1.1:
+        # 1 message per second case
+        diff = now - self.users[chat_id][0]
+        if diff <= 1:
             return False
-        self.users[chat_id] = now
-        return True
+        self.users[chat_id][0] = now
+
+        # 20 messages per minute case
+        diff = now - self.users[chat_id][1]
+        if diff <= 60:
+            msg_count = self.users[chat_id][2]
+            if msg_count < 20:
+                return True
+            else:
+                return False
+        else:
+            self.users[chat_id][1] = now
+            self.users[chat_id][2] = 0
+            return True
+
+    def send_to(self, chat_id):
+        if not chat_id in self.users:
+            raise Exception("invalid usage of rate limiter")
+        self.users[chat_id][2] += 1
